db96bf1372634a0686b6d0137cbcbb0b5cac10e4
KLeonGreen <114649634+LeonMagnificat@users.noreply.github.com>
2023-03-07 15:44:04 +0100

    invitation to group

--- src/api/user/user.js
+++ src/api/user/user.js
@@ -1,5 +1,6 @@
 import express from "express";
 import UserModel from "./model.js";
+import GroupModel from "../group/model.js";
 import bcrypt from "bcrypt";
 import { createAccessToken } from "../../library/Auth/tokenTools.js";
 
@@ -47,6 +48,49 @@ userRouter.post("/login", async (req, res, next) => {
     const accessToken = await createAccessToken(payload);
 
     res.status(200).send({ message: "Logged In successfully", user: user, accessToken });
+  } catch (error) {
+    next(error);
+  }
+});
+
+userRouter.post("/register/:groupId", async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await UserModel.findOne({ email });
+    //if user exist
+    if (user) {
+      return res.status(400).send({ message: "User already exists" });
+    }
+    //if user do not exist, create one
+    const newUser = new UserModel(req.body);
+
+    //Hash password before saving it
+    const hashedPassword = await bcrypt.hash(password, 11);
+    newUser.password = hashedPassword;
+    await newUser.save();
+
+    //create token
+
+    const payload = { _id: newUser._id, role: newUser.role };
+    const accessToken = await createAccessToken(payload);
+
+    const groupId = req.params.groupId;
+    const userId = newUser._id;
+    console.log("groupId", groupId, "userId", userId);
+
+    const group = await GroupModel.findById(groupId);
+    if (!group) {
+      return res.status(404).json({ message: "Group not found" });
+    }
+    if (group.members.includes(userId)) {
+      return res.status(400).json({ message: "User is already in group" });
+    }
+
+    group.members.push(userId);
+    await group.save();
+
+    res.status(200).send({ group: group, accessToken });
   } catch (error) {
     next(error);
   }
